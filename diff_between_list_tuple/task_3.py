4e305fcab1d291caad9db707bf4eb7d0c8266739
m-onerl <monerl2525@gmail.com>
2025-12-04 15:20:32 +0100

    add: diff between tuple and list

---
+++ diff_between_list_tuple/task_3.py
@@ -0,0 +1,9 @@
+# create code where we can check the most diffrence between tuple and list:
+
+L = [1, 2, 3, True, (1, 2)]
+T = (4, 5, 6, False, ['x', 'y'])
+
+L[2] = 'X'
+print(L)
+T[3] = 'X'
+print(T)

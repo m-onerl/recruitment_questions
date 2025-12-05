73a59f4deb89a6303f38602705445c17e3bacdab
m-onerl <monerl2525@gmail.com>
2025-12-05 15:44:30 +0100

    add: what values can be key

---
+++ what_can_be_key/task_7.py
@@ -0,0 +1,7 @@
+# Which one from these dict are maded wrong?
+
+A = {1: 1, 2: 4, 3: 9}
+B = {'imie': 'Anna', 'nazwisko': 'Kowalska'}
+# C = {[4, 5]: [16, 25]}
+D = {(4, 5): (16, 25)}
+# E = {{1: 2}: "jeden_dwa"}

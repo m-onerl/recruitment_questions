fbe72a0862cb60a4031faefc72ebefd98f54bcb0
m-onerl <monerl2525@gmail.com>
2025-12-06 21:47:14 +0100

    add: added the exercise where i looking for some key in dict

--- do_key_is_in_dict/task_9.py
+++ do_key_is_in_dict/task_9.py
@@ -1,14 +1,27 @@
 # for string x make a dict
 # check how many time letter is in string
 
-x = "myszydokazujakiedykotanieczuja"
+# x = "myszydokazujakiedykotanieczuja"
 
-D = {}
+# D = {}
 
-for letter in x:
-    if letter not in D.keys():
-        D[letter] = 1
-    else:
-        D[letter] += 1
+# for letter in x:
+#     if letter not in D.keys():
+#         D[letter] = 1
+#     else:
+#         D[letter] += 1
 
-print(D)
+# print(D)
+
+S = {x: x+1 for x in range(10000) if x % 23 == 0}
+
+
+def check_numbers():
+    for number in S:
+        if number in S.keys() == 7340:
+            return True
+        else:
+            return False
+
+
+print(check_numbers())
